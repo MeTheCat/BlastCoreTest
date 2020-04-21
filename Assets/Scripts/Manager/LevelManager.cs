8d946e4927ff5d46d34dcc1f4abbb979f6a0dbfd
SpaceKitty <spacekitty135@gmail.com>
2020-04-22 00:46:58 +0300

    feat(level-manager): To keep track of active enemies

---
+++ Assets/Scripts/Manager/LevelManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Keeps track of the level state
+/// </summary>
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager Instance;
+
+    public UnityEvent LevelClearedEvent = new UnityEvent();
+
+    public UnityEvent EnemyKilledEvent = new UnityEvent();
+
+    #region PrivateFields
+    [SerializeField]
+    private int levelClearedAtEnemiesLeft;
+
+    [ReadOnly, SerializeField]
+    private int enemiesCount = 0;
+
+    [ReadOnly, SerializeField]
+    private int blastCoreCount = 0;
+    #endregion
+
+    public int GetTotalEnemiesCount()
+    {
+        return enemiesCount;
+    }
+
+    public int GetBlastCoreCount()
+    {
+        return blastCoreCount;
+    }
+
+    public void OnEnemySpawned(EnemyType enemyType)
+    {
+        enemiesCount++;
+    }
+
+    public void OnEnemyDied(EnemyType enemyType)
+    {
+        enemiesCount--;
+
+        EnemyKilledEvent.Invoke();
+
+        if (enemiesCount < levelClearedAtEnemiesLeft)
+        {
+            LevelClearedEvent.Invoke();
+        }
+    }
+
+    #region Setup
+    private void Awake()
+    {
+        Instance = this;
+    }
+    #endregion
+}
